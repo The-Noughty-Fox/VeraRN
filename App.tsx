dd725104b99a16b2d1f2f4e4bedd4ad5401717dc
Alex Culeva <culevalex@gmail.com>
2023-11-01 19:12:07 +0200

    Handle messages from Vera

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { SafeAreaView, StatusBar, Text } from "react-native";
+import { DirectEventHandler } from "react-native/Libraries/Types/CodegenTypes";
 import RTNVera from 'vera-rtn-sdk/js/index';
-import type {VeraConfiguration} from 'vera-rtn-sdk/js/RTNVeraNativeComponent'
+import {VeraView, type VeraConfiguration, type VeraMessage} from 'vera-rtn-sdk/js/RTNVeraNativeComponent'
 
 const App: () => JSX.Element = () => {
   const config: VeraConfiguration = {
@@ -12,11 +13,18 @@ const App: () => JSX.Element = () => {
     domain: 'https://vera.resonai.com',
     language: 'ru'
   }
+  const handleMessage: DirectEventHandler<VeraMessage> = (event) => {
+    console.log(`Got event ${event.nativeEvent}`)
+  }
 
   return (
     <SafeAreaView>
     <StatusBar barStyle={'dark-content'} />
-    <RTNVera style={{width: '100%', height: '100%'}} config={config}/>
+    <VeraView 
+      style={{width: '100%', height: '100%'}} 
+      config={config}
+      onHandleMessage={handleMessage}
+      />
     </SafeAreaView>
   )
 }
