3b5932f8aa27887ed058780f4562351c7cbf8639
Alex Culeva <culevalex@gmail.com>
2023-11-01 10:24:13 +0200

    Example app of passing configuration to Vera

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import RTNVera from 'vera-rtn-sdk/js/index';
 
+AppRegistry.registerComponent("RTNVera", () => RTNVera)
 AppRegistry.registerComponent(appName, () => App);
